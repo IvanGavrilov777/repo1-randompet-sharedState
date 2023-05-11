e9bc71722ea60341eddd4873f378d6a4fd2459ed
IvanGavrilov777 <125308112+IvanGavrilov777@users.noreply.github.com>
2023-05-11 19:17:59 +0300

    Create random-pet.tf

---
+++ random-pet.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+}
+
+resource "random_pet" "server" {
+  keepers = {
+    time = timestamp()
+  }
+}
+
+output "value" {
+  value = "New_Pet_is_called_${random_pet.server.id}"
+}
